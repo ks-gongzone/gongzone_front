136bcac19d306d391bce4089ca6880f22145e6fa
hanokfairy <hanokfairy@gmail.com>
2024-06-11 17:15:43 +0900

    [GZ-9]:sparkles:feat:Main layout added

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -1,10 +1,14 @@
 import MainSlider from "../../components/home/MainSlider";
+import MainMap from "../../components/page/home/MainMap";
+import SubSlider from "../../components/page/home/SubSlider";
 import MainCardSection from "./MainCardSection";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center box-border">
       <MainSlider />
+      <MainMap />
+      <SubSlider />
       <MainCardSection />
     </div>
   );
