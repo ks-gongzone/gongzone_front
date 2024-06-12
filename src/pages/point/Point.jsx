274f14576a395ea97be9bfadab1969ff2d42b5f9
wsk221e <wsk221e@gmail.com>
2024-06-12 20:46:57 +0900

    [GZ-12] :sparkles:feat: modify point page - default layout
    - Point.jsx: implemented default layout

--- src/pages/point/Point.jsx
+++ src/pages/point/Point.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Section from "../../layouts/Section";
 
 export default function Point() {
@@ -7,9 +7,28 @@ export default function Point() {
   return (
     <Section>
       <div className={"bg-gray-50 w-full p-8"}>
-        <h2 className={"text-xl"}>포인트 페이지</h2>
+        <h2 className={"text-xl font-bold ml-8"}>포인트 페이지</h2>
         <hr className={"my-4"} />
-        <p>포인트 페이지 메인</p>
+        <div className={"flex flex-grow"}>
+          <div className={"flex justify-center w-1/4"}>
+            보유 포인트
+          </div>
+          <div className={"flex justify-center w-1/4"}>
+            <Link to={`/member/${memberNo}/point/history`} className="text-blue-500 underline">
+              포인트 내역
+            </Link>
+          </div>
+          <div className={"flex justify-center w-1/4"}>
+            <Link to={`/member/${memberNo}/point/charge`} className="text-blue-500 underline">
+              포인트 충전
+            </Link>
+          </div>
+          <div className={"flex justify-center w-1/4"}>
+            <Link to={`/member/${memberNo}/point/withdraw`} className="text-blue-500 underline">
+              포인트 인출
+            </Link>
+          </div>
+        </div>
       </div>
     </Section>
   );
