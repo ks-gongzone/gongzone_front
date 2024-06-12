66e8df77d2abc19e014b6d1d00413fdcb3965b3f
wsk221e <wsk221e@gmail.com>
2024-06-12 21:12:36 +0900

    [GZ-12] :sparkles:feat: implemented member current point layout

--- src/layouts/Section.jsx
+++ src/layouts/Section.jsx
@@ -1,7 +1,11 @@
-export default function Section({ children }) {
+export default function Section({ children, title}) {
   return (
-    <div id={"section"} className={"flex flex-grow my-8 mx-12"}>
-      {children}
+    <div id={"section"} className={"flex flex-grow bg- my-8 mx-12"}>
+      <div className={"bg-gray-50 w-full p-8"}>
+        <h2 className={"text-xl font-bold ml-8"}>{title}</h2>
+        <hr className={"my-4"} />
+        {children}
+      </div>
     </div>
   );
 }
--- src/pages/point/Point.jsx
+++ src/pages/point/Point.jsx
@@ -1,35 +1,38 @@
 import { Link, useParams } from "react-router-dom";
-import Section from "../../layouts/Section";
+import PointSection from "../../layouts/Section";
 
-export default function Point() {
+export default/**
+*
+*/
+ function Point() {
   const { memberNo } = useParams();
+  const title = `${memberNo}님의 포인트 페이지`;
 
   return (
-    <Section>
-      <div className={"bg-gray-50 w-full p-8"}>
-        <h2 className={"text-xl font-bold ml-8"}>포인트 페이지</h2>
-        <hr className={"my-4"} />
-        <div className={"flex flex-grow"}>
-          <div className={"flex justify-center w-1/4"}>
-            보유 포인트
-          </div>
-          <div className={"flex justify-center w-1/4"}>
-            <Link to={`/member/${memberNo}/point/history`} className="text-blue-500 underline">
-              포인트 내역
-            </Link>
-          </div>
-          <div className={"flex justify-center w-1/4"}>
-            <Link to={`/member/${memberNo}/point/charge`} className="text-blue-500 underline">
-              포인트 충전
-            </Link>
-          </div>
-          <div className={"flex justify-center w-1/4"}>
-            <Link to={`/member/${memberNo}/point/withdraw`} className="text-blue-500 underline">
-              포인트 인출
-            </Link>
+    <PointSection title={title}>
+      <div className={"flex flex-grow"}>
+        <div className={"flex flex-col justify-center items-center w-1/4"}>
+          보유 포인트
+          <div className={"flex justify-center items-center text-xl bg-gray-100 w-32 h-32"}>
+            <span className={"font-bold"}>?</span><span>P</span>
           </div>
         </div>
+        <div className={"flex justify-center w-1/4"}>
+          <Link to={`/member/${memberNo}/point/history`} className="text-blue-500 underline">
+            포인트 내역
+          </Link>
+        </div>
+        <div className={"flex justify-center w-1/4"}>
+          <Link to={`/member/${memberNo}/point/charge`} className="text-blue-500 underline">
+            포인트 충전
+          </Link>
+        </div>
+        <div className={"flex justify-center w-1/4"}>
+          <Link to={`/member/${memberNo}/point/withdraw`} className="text-blue-500 underline">
+            포인트 인출
+          </Link>
+        </div>
       </div>
-    </Section>
+    </PointSection>
   );
 }
