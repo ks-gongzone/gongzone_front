1d0fd0c44149ed582f416cdf67d242a6711acd0a
hanokfairy <hanokfairy@gmail.com>
2024-06-30 23:21:35 +0900

    [GZ-9]✨feat:layout detail added
    레이아웃 디테일 추가

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,10 +1,11 @@
 import { Link, useNavigate } from "react-router-dom";
-import SearchBar from "../components/page/home/SearchBar";
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import ModalLogin from "../components/modal/ModalLogin";
 import ModalSignup from "../components/modal/ModalRegister";
 import AuthStore from "../utils/zustand/AuthStore";
 import ModalStore from "../utils/zustand/ModalStore";
+import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+import logo from "../../src/assets/logo/GZlogo.PNG";
 
 export default function LayoutHeader() {
   const { isLogin, statusLogout, statusLogin, setIsLogin } = AuthStore();
@@ -16,6 +17,9 @@ export default function LayoutHeader() {
   } = ModalStore();
   const navigate = useNavigate();
 
+  const [activeTab, setActiveTab] = useState(null);
+  const borderRef = useRef();
+
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
     if (token) {
@@ -23,68 +27,96 @@ export default function LayoutHeader() {
     }
   }, []);
 
-  const handleMyPage = () => {
-    navigate("/myPage");
+  const handleTabHover = (e) => {
+    const { offsetLeft, offsetWidth } = e.target;
+    borderRef.current.style.transform = `translateX(${offsetLeft}px)`;
+    borderRef.current.style.width = `${offsetWidth}px`;
+  };
+
+  const handleTabLeave = () => {
+    borderRef.current.style.width = "0px";
   };
 
   return (
     <div className="shadow-md z-10">
-      <div className="flex items-center h-16 px-20 justify-between">
-        <div className="w-full flex items-center max-w-6xl mx-auto gap-20">
-          <Link className="text-2xl" to="/">
-            LOGO
+      <div className="flex items-center h-16 relative">
+        <div className="flex items-center max-w-5xl mx-auto gap-4 justify-between flex-wrap w-full">
+          <Link className="text-2xl whitespace-nowrap" to="/">
+            <img alt="logo" src={logo} className="h-10 pr-20" />
           </Link>
-          <div className="w-96 text-[15px] justify-around items-center flex">
-            <Link className="hover:text-red-600" to="/api/boards/list">
+          <div className="flex flex-grow justify-start text-[15px] whitespace-nowrap font-semibold relative py-2">
+            <div
+              ref={borderRef}
+              className="absolute bottom-0 h-0.5 bg-red-600 transition-all duration-300"
+              style={{ width: "0px", transform: "translateX(0px)" }}
+            ></div>
+            <Link
+              onMouseEnter={handleTabHover}
+              onMouseLeave={handleTabLeave}
+              className="hover:text-red-600 px-5"
+              to="/api/boards/list"
+            >
               게시판
             </Link>
-            <Link className="hover:text-red-600" to="/">
+            <div className="px-8"></div>
+            <Link
+              onMouseEnter={handleTabHover}
+              onMouseLeave={handleTabLeave}
+              className="hover:text-red-600 px-5"
+              to="/"
+            >
               파티
             </Link>
-            <Link className="hover:text-red-600" to="/">
+            <div className="px-8"></div>
+            <Link
+              onMouseEnter={handleTabHover}
+              onMouseLeave={handleTabLeave}
+              className="hover:text-red-600 px-5"
+              to="/"
+            >
               공지사항
             </Link>
           </div>
-          <div>
-            <SearchBar />
-          </div>
-          <div>
+          <button
+            onClick={() => navigate("/api/boards/list")}
+            className="flex items-center bg-red-400 text-white px-4 py-2 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 whitespace-nowrap"
+          >
+            <MagnifyingGlassIcon className="w-5 h-5 mr-2" />
+            검색
+          </button>
+          <div className="flex items-center">
             {isLogin ? (
-              <>
-                <div className="flex items-center">
-                  <button
-                    onClick={handleMyPage}
-                    className="text-[10px] bg-white text-blue-500 py-2 px-2 rounded-l-lg hover:bg-gray-200"
-                  >
-                    마이페이지
-                  </button>
-                  <span className="text-[10px] text-blue-500 px-1">/</span>
-                  <button
-                    onClick={statusLogout}
-                    className="text-[10px] bg-white text-blue-500 px-2 py-2 rounded-r-lg hover:bg-gray-200"
-                  >
-                    로그아웃
-                  </button>
-                </div>
-              </>
+              <div className="flex items-center whitespace-nowrap">
+                <button
+                  onClick={() => navigate("/myPage")}
+                  className="text-[10px] bg-white text-blue-500 py-2 px-2 rounded-l-lg hover:bg-gray-200"
+                >
+                  마이페이지
+                </button>
+                <span className="text-[10px] text-blue-500 px-1">/</span>
+                <button
+                  onClick={statusLogout}
+                  className="text-[10px] bg-white text-blue-500 px-2 py-2 rounded-r-lg hover:bg-gray-200"
+                >
+                  로그아웃
+                </button>
+              </div>
             ) : (
-              <>
-                <div className="flex items-center">
-                  <button
-                    onClick={() => setIsModalOpen(true)}
-                    className="text-[10px] bg-white text-blue-500 px-2 py-2 rounded-l-lg hover:bg-gray-200"
-                  >
-                    로그인
-                  </button>
-                  <span className="text-[10px] text-blue-500 px-1">/</span>
-                  <button
-                    onClick={() => setIsRegisterModalOpen(true)}
-                    className="text-[10px] bg-white text-blue-500 px-2 py-2 rounded-r-lg hover:bg-gray-200"
-                  >
-                    가입
-                  </button>
-                </div>
-              </>
+              <div className="flex items-center whitespace-nowrap">
+                <button
+                  onClick={() => setIsModalOpen(true)}
+                  className="text-[10px] bg-white text-blue-500 px-2 py-2 rounded-l-lg hover:bg-gray-200"
+                >
+                  로그인
+                </button>
+                <span className="text-[10px] text-blue-500 px-1">/</span>
+                <button
+                  onClick={() => setIsRegisterModalOpen(true)}
+                  className="text-[10px] bg-white text-blue-500 px-2 py-2 rounded-r-lg hover:bg-gray-200"
+                >
+                  가입
+                </button>
+              </div>
             )}
             <ModalLogin
               isOpen={isModalOpen}
