4f85d381fc964017061d61a6314686c4703d0fc1
wsk221e <wsk221e@gmail.com>
2024-07-02 11:30:31 +0900

    [GZ-12] :recycle: refactor: move wrapping section from main page to history page

--- src/pages/point/PointHistory.jsx
+++ src/pages/point/PointHistory.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import GZAPI from "../../utils/api";
 import { formatNumber } from "../../libs/utilities";
 import State from "../../utils/state/State";
+import PointInnerSection from "../../components/page/point/PointInnerSection";
 
 export default function PointHistory({ pointNo, isLoaded }) {
   const pointHistory = State("pointHistory", []);
@@ -19,44 +20,46 @@ export default function PointHistory({ pointNo, isLoaded }) {
 
 
   return (
-    <div className="flex flex-col flex-grow space-y-4">
-      <div className="flex justify-around">
-        <button className="w-1/4 h-8 rounded-lg bg-gray-300">
-          !사용 내역
-        </button>
-        <button className="w-1/4 h-8 rounded-lg bg-gray-300">
-          !충전(증가) 내역
-        </button>
-      </div>
-      <div className="flex flex-col w-full">
-        <div className="flex w-full bg-gray-100 text-center">
-          <div className="w-2/12 border-x border-x-gray-300 border-y-2 border-y-gray-400">
-            내역번호
-          </div>
-          <div className="w-4/12 border-x border-x-gray-300 border-y-2 border-y-gray-400">
-            일시
-          </div>
-          <div className="w-2/12 border-x border-x-gray-300 border-y-2 border-y-gray-400">
-            유형
-          </div>
-          <div className="w-[12%] border-x border-x-gray-300 border-y-2 border-y-gray-400">
-            변동전
-          </div>
-          <div className="w-[12%] border-x border-x-gray-300 border-y-2 border-y-gray-400">
-            변동량
-          </div>
-          <div className="w-[12%] border-x border-x-gray-300 border-y-2 border-y-gray-400">
-            변동후
-          </div>
-          <div className="w-[12%] border-x border-x-gray-300 border-y-2 border-y-gray-400">
-            상태
-          </div>
+    <PointInnerSection title={ "포인트 내역" }>
+      <div className="flex flex-col flex-grow space-y-4">
+        <div className="flex justify-around">
+          <button className="w-1/4 h-8 rounded-lg bg-gray-300">
+            !사용 내역
+          </button>
+          <button className="w-1/4 h-8 rounded-lg bg-gray-300">
+            !충전(증가) 내역
+          </button>
         </div>
-        { pointHistory.value.map((history, index) => {
-          return <PointHistoryRow key={ index } pointHistory={ history } />;
-        }) }
+        <div className="flex flex-col w-full">
+          <div className="flex w-full bg-gray-100 text-center">
+            <div className="w-2/12 border-x border-x-gray-300 border-y-2 border-y-gray-400">
+              내역번호
+            </div>
+            <div className="w-4/12 border-x border-x-gray-300 border-y-2 border-y-gray-400">
+              일시
+            </div>
+            <div className="w-2/12 border-x border-x-gray-300 border-y-2 border-y-gray-400">
+              유형
+            </div>
+            <div className="w-[12%] border-x border-x-gray-300 border-y-2 border-y-gray-400">
+              변동전
+            </div>
+            <div className="w-[12%] border-x border-x-gray-300 border-y-2 border-y-gray-400">
+              변동량
+            </div>
+            <div className="w-[12%] border-x border-x-gray-300 border-y-2 border-y-gray-400">
+              변동후
+            </div>
+            <div className="w-[12%] border-x border-x-gray-300 border-y-2 border-y-gray-400">
+              상태
+            </div>
+          </div>
+          { pointHistory.value.map((history, index) => {
+            return <PointHistoryRow key={ index } pointHistory={ history } />;
+          }) }
+        </div>
       </div>
-    </div>
+    </PointInnerSection>
   );
 }
 
