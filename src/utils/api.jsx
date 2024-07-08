0d4e39f63c890e61ec4f12820d188b7dea1c8b5d
Huroad <cabin9999@naver.com>
2024-07-08 09:23:00 +0900

    [GZ-30]feat:refresh token

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -7,9 +7,6 @@ const GZAPI = axios.create({
   },
 });
 
-// 아래 주석은 예시 -> 디벨롭해서 개발이 필요합니다.
-// const Token = window.localStorage.getItem("accessToken") || null;
-// login token
 GZAPI.interceptors.request.use(
     (config) => {
       const token = window.localStorage.getItem("accessToken");
@@ -23,27 +20,6 @@ GZAPI.interceptors.request.use(
       return Promise.reject(error);
     }
 );
-
-// if (Token) {
-//   GZAPI.defaults.headers.Authorization = "Bearer " + Token;
-// }
-
-/*GZAPI.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      //console.log(error);
-      if (error.response) {
-        if (error.response.status === 401) {
-          window.localStorage.removeItem("accessToken");
-          const url = window.location.href;
-          window.localStorage.setItem("redirect", url);
-          //window.location.href = "/";
-        }
-      }
-      // error.response -> error로 변경
-      return Promise.reject(error);
-    }
-);*/
 
 GZAPI.interceptors.response.use(
   (response) => response,
