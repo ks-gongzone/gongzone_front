04cb7080b878a0c28485963a66bea4e49299610f
wsk221e <wsk221e@gmail.com>
2024-07-09 14:29:39 +0900

    [GZ-12] :sparkles: feat: add pagination button to point history page

--- src/pages/point/PointMain.jsx
+++ src/pages/point/PointMain.jsx
@@ -3,9 +3,9 @@ import { PointInnerSection, PointSection } from "../../components/page/point/Ind
 import { formatNumber } from "../../libs/utilities";
 import GZAPI from "../../utils/api";
 import State from "../../utils/state/State";
-import { PointHistory } from "./Index";
 import { Link } from "react-router-dom";
 import { usePointData } from "./context/PointContext";
+import { PointHistoryTable } from "./PointHistory";
 
 
 export default function Point() {
@@ -25,31 +25,35 @@ export default function Point() {
 
 
   return (
-      <PointSection title={ title }>
-        <div className="flex flex-col flex-grow space-y-12">
-          {/* 보유 포인트, 충전/인출 버튼 */ }
-          <PointInnerSection title={ "보유 포인트" }>
-            <div className="items-center w-full text-right text-6xl">
-              <span className="font-bold">{ formatNumber(memberPoint.value) }</span>
-              <span className="ml-4 text-3xl">Point</span>
-            </div>
-            <div className="flex justify-end space-x-4">
-              <Link to="/myPage/point/charge"
-                    className="box-border border-2 rounded-xl px-4 py-2 bg-gray-400"
-              >
-                충전
-              </Link>
-              <Link to="/myPage/point/withdraw"
-                    className="box-border border-2 rounded-xl px-4 py-2 bg-gray-300"
-              >
-                인출
-              </Link>
-            </div>
-          </PointInnerSection>
+    <PointSection title={ title }>
+      <div className="flex flex-col flex-grow space-y-12">
+        {/* 보유 포인트, 충전/인출 버튼 */ }
+        <PointInnerSection title={ "보유 포인트" }>
+          <div className="items-center w-full text-right text-6xl">
+            <span className="font-bold">{ formatNumber(memberPoint.value) }</span>
+            <span className="ml-4 text-3xl">Point</span>
+          </div>
+          <div className="flex justify-end space-x-4">
+            <Link to="/myPage/point/charge"
+                  className="box-border border-2 rounded-xl px-4 py-2 bg-gray-400"
+            >충전</Link>
+            <Link to="/myPage/point/withdraw"
+                  className="box-border border-2 rounded-xl px-4 py-2 bg-gray-300"
+            >인출</Link>
+          </div>
+        </PointInnerSection>
 
-          {/* 포인트 내역*/ }
-          <PointHistory pointNo={ pointNo } />
-        </div>
-      </PointSection>
+        {/* 포인트 내역*/ }
+        <PointInnerSection title={ "포인트 내역" }
+                           description={ "(!현재 페이지에는 최근 10건만 표시됩니다.)" }>
+          <PointHistoryTable pointNo={ pointNo } />
+          <div className="flex justify-center">
+            <Link to="/myPage/point/history"
+                  className="w-1/5 text-center box-border border-2 rounded-xl px-4 py-2 bg-gray-400"
+            >내역 더보기</Link>
+          </div>
+        </PointInnerSection>
+      </div>
+    </PointSection>
   );
 }
