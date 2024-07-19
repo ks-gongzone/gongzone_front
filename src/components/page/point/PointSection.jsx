c54a77f1cddc8b576b29fef8e1c7a0bca0c54611
hanokfairy <hanokfairy@gmail.com>
2024-07-19 16:27:56 +0900

    [GZ-39]🎨design:point page design added
    포인트 페이지 일부 디자인 적용

--- src/components/page/point/PointSection.jsx
+++ src/components/page/point/PointSection.jsx
@@ -2,26 +2,27 @@ import { Link } from "react-router-dom";
 
 export default function PointSection({ children, title, renderPage }) {
   return (
-
-      <div
-          id={ "section" }
-          className="flex flex-grow justify-center
+    <div
+      id={"section"}
+      className="flex flex-grow justify-center
                                      h-min-[45vh]
-                                     my-16 mx-24"
-      >
-        <div
-            className="flex flex-grow
+                                     mb-12 mx-10"
+    >
+      <div
+        className="flex flex-grow
                       w-[75vw] min-w-[650px] max-w-[1200px]"
-        >
-          <div className="bg-gray-50 w-full p-8">
-            <div className="flex space-x-4">
-              <Link to="/myPage/point" className="items-center text-3xl">&lt;</Link>
-              <h2 className="font-bold text-2xl">{ title }</h2>
-            </div>
-            <hr className="my-12" />
-            { children }
+      >
+        <div className="w-full p-8 flex flex-col">
+          <div className="flex space-x-4">
+            <Link to="/myPage/point" className="items-center text-3xl">
+              &lt;
+            </Link>
+            <h2 className="font-bold text-2xl">{title}</h2>
           </div>
+          <hr className="my-12" />
+          {children}
         </div>
       </div>
+    </div>
   );
 }
